9e4e5c104af0399c8c074e241c8ee1a91a868103
konstantin kochetov <kochetov0824@gmail.com>
2020-06-19 13:46:09 +0200

    added loader

---
+++ src/main/java/com/konstantinkochetov/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,19 @@
+package com.konstantinkochetov.msscbeerservice.repositories;
+
+import com.konstantinkochetov.msscbeerservice.domain.Beer;
+import com.konstantinkochetov.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+    Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
+
+    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
+
+    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
+
+    Beer findByUpc(String upc);
+}
