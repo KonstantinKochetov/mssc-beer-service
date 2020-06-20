a37be605e71a3fcad54ac289aef4342dd3df6628
konstantin kochetov <kochetov0824@gmail.com>
2020-06-20 13:25:34 +0200

    two services communication

--- src/main/java/com/konstantinkochetov/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/konstantinkochetov/msscbeerservice/web/controller/BeerController.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
--- src/main/java/com/konstantinkochetov/msscbeerservice/web/mappers/BeerMapperDecorator.java
+++ src/main/java/com/konstantinkochetov/msscbeerservice/web/mappers/BeerMapperDecorator.java
@@ -1,35 +1,38 @@
 package com.konstantinkochetov.msscbeerservice.web.mappers;
 
+import com.konstantinkochetov.msscbeerservice.domain.Beer;
+import com.konstantinkochetov.msscbeerservice.services.inventory.BeerInventoryService;
+import com.konstantinkochetov.msscbeerservice.web.model.BeerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
-//public abstract class BeerMapperDecorator implements BeerMapper {
-//    private BeerInventoryService beerInventoryService;
-//    private BeerMapper mapper;
-//
-//    @Autowired
-//    public void setBeerInventoryService(BeerInventoryService beerInventoryService) {
-//        this.beerInventoryService = beerInventoryService;
-//    }
-//
-//    @Autowired
-//    public void setMapper(BeerMapper mapper) {
-//        this.mapper = mapper;
-//    }
-//
-//    @Override
-//    public BeerDto beerToBeerDto(Beer beer) {
-//        return mapper.beerToBeerDto(beer);
-//    }
-//
-//    @Override
-//    public BeerDto beerToBeerDtoWithInventory(Beer beer) {
-//        BeerDto dto = mapper.beerToBeerDto(beer);
-//        dto.setQuantityOnHand(beerInventoryService.getOnhandInventory(beer.getId()));
-//        return dto;
-//    }
-//
-//    @Override
-//    public Beer beerDtoToBeer(BeerDto beerDto) {
-//        return mapper.beerDtoToBeer(beerDto);
-//    }
-//}
+public abstract class BeerMapperDecorator implements BeerMapper {
+    private BeerInventoryService beerInventoryService;
+    private BeerMapper mapper;
+
+    @Autowired
+    public void setBeerInventoryService(BeerInventoryService beerInventoryService) {
+        this.beerInventoryService = beerInventoryService;
+    }
+
+    @Autowired
+    public void setMapper(BeerMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        return mapper.beerToBeerDto(beer);
+    }
+
+    @Override
+    public BeerDto beerToBeerDtoWithInventory(Beer beer) {
+        BeerDto dto = mapper.beerToBeerDto(beer);
+        dto.setQuantityOnHand(beerInventoryService.getOnhandInventory(beer.getId()));
+        return dto;
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto) {
+        return mapper.beerDtoToBeer(beerDto);
+    }
+}
