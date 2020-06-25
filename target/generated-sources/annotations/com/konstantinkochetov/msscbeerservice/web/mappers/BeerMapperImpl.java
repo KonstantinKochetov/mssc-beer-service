325a16357368e1b3a86efdf41754f10c28e856e1
konstantin kochetov <kochetov0824@gmail.com>
2020-06-25 14:25:36 +0200

    fixed jms failure

--- target/generated-sources/annotations/com/konstantinkochetov/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/konstantinkochetov/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-06-21T11:15:54+0200",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.5 (AdoptOpenJDK)"
+    date = "2020-06-25T14:09:09+0200",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.5 (AdoptOpenJDK)"
 )
 @Component
 @Primary
