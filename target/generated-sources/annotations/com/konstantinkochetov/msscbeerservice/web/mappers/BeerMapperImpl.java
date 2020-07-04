2bfafdd1c0ae9fa230118fbcd324beef504bd83d
konstantin kochetov <kochetov0824@gmail.com>
2020-07-04 14:07:54 +0200

    services working together

--- target/generated-sources/annotations/com/konstantinkochetov/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/konstantinkochetov/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-06-28T13:39:26+0200",
+    date = "2020-07-04T13:47:03+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.5 (AdoptOpenJDK)"
 )
 @Component
